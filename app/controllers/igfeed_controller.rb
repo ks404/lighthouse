30c1f3d1362c19b5ff3586813456dc8bf9408c15
ks404 <ksajaev@gmail.com>
2016-11-17 06:42:31 -0600

    got rid of igfeed

--- app/controllers/igfeed_controller.rb
+++ app/controllers/igfeed_controller.rb
@@ -1,16 +1,16 @@
-class IgfeedController < ApplicationController
+#class IgfeedController < ApplicationController
   
 
-  def index
+#  def index
 
-    ACCESS_TOKEN = "326630920.f5aa423.e195e8fdb7414a69a6cda7431afbefce"
-    client = "https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=#{ACCESS_TOKEN}" 
+#    ACCESS_TOKEN = "326630920.f5aa423.e195e8fdb7414a69a6cda7431afbefce"
+#    client = "https://api.instagram.com/v1/tags/nofilter/media/recent?access_token=#{ACCESS_TOKEN}" 
 
     
 
 
 
-  end
+#  end
 
 
 
@@ -18,4 +18,4 @@ class IgfeedController < ApplicationController
 
 
 
-end
+#end
